9a6c0362daf03982fc93beb70e9e80743ec97d1b
dave20100 <dave201000@gmail.com>
2021-02-25 12:24:12 +0100

    fix

--- src/app/esp.service.ts
+++ src/app/esp.service.ts
@@ -2,7 +2,6 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Device } from './model/device';
-import { map } from 'rxjs/operators';
 
 @Injectable()
 export class EspService {
